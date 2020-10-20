9f9238da3dd3eaf9de53fbd4dc5b0325cf33232d
Bennett <bennybenester@gmail.com>
2020-10-20 11:20:27 +0300

    :sparkles: add helpers to create uid sanitization
    I added helpers to sanitize response payload and create uids on the fly

---
+++ src/core/helpers/genera-uid.ts
@@ -0,0 +1,12 @@
+export function generateUid() {
+    let uidGenerated = '';
+    const characters =
+      '123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    for (let i = 0; i < 13; i++) {
+      uidGenerated += characters.charAt(
+        Math.floor(Math.random() * characters.length),
+      );
+    }
+    return uidGenerated;
+  }
+  
